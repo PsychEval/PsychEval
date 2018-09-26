90976d9f96dbe7f7a4ea0b7f62459b2fc9a5f44f
Bryan Chiou <bryanchiou97@gmail.com>
2018-09-26 00:12:29 -0400

    Changed UI to javafx. Functionality stayed same

--- MainModule/src/Account/EditPassword.java
+++ MainModule/src/Account/EditPassword.java
@@ -1,109 +1,119 @@
+package Account;
 
-import javax.swing.*;
-import java.awt.*;        // Using AWT container and component classes
-import java.awt.event.*;  // Using AWT event classes and listener interfaces
-import javax.swing.border.EmptyBorder;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import javafx.scene.text.Text;
 
-public class EditPassword extends Frame implements ActionListener {
+public class EditPassword extends Application{
 
-    private Label enterOldPass;
-    private Label enterNewPass;
-    private Label reenterNewPass;
-    private JPasswordField enterOldPassword;
-    private JPasswordField enterNewPassword;
-    private JPasswordField reenterNewPassword;
-    private Button submitPassword;
-    private String submittedPassword;
-    private Label successMessage;
-    private JFrame frame;
-    private JPanel windowPane;
-    private BoxLayout boxLayout;
-
-    private void makeFrame() {
-        JFrame.setDefaultLookAndFeelDecorated(true);
-        frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        windowPane = new JPanel();
-        boxLayout = new BoxLayout(windowPane, BoxLayout.Y_AXIS);
-        windowPane.setLayout(boxLayout);
-        windowPane.setBorder(new EmptyBorder(new Insets(150, 200, 150, 200)));
-
-
-        enterOldPass = new Label("Enter Old Password:");
-        enterNewPass = new Label("Enter New Password:");
-        reenterNewPass = new Label("Reenter New Password:");
-        enterOldPassword = new JPasswordField(20);
-        enterNewPassword = new JPasswordField(20);
-        reenterNewPassword = new JPasswordField(20);
-//        enterOldPassword.setEditable(true);
-//        enterNewPassword.setEditable(true);
-//        reenterNewPassword.setEditable(true);
-
-        windowPane.add(enterOldPass);
-        windowPane.add(enterOldPassword);
-        windowPane.add(enterNewPass);
-        windowPane.add(enterNewPassword);
-        windowPane.add(reenterNewPass);
-        windowPane.add(reenterNewPassword);
-
-        successMessage = new Label("");
-        successMessage.setPreferredSize(new Dimension(275, 50));
-        successMessage.setVisible(true);
-        windowPane.add(successMessage);
-
-        submitPassword = new Button("Submit");
-        windowPane.add(submitPassword);
-        submitPassword.addActionListener(this);
-
-        frame.add(windowPane);
-        frame.pack();
-        frame.setVisible(true);
+    private GridPane createFormPane(){
+        //n rows 2 colums
+        GridPane gp = new GridPane();
+        gp.setAlignment(Pos.CENTER);
+        gp.setPadding(new Insets(40,40,40,40));
+        gp.setHgap(10);
+        gp.setVgap(10);
+        ColumnConstraints columnOneConstraints = new ColumnConstraints(200, 200, Double.MAX_VALUE);
+        columnOneConstraints.setHalignment(HPos.RIGHT);
+        ColumnConstraints columnTwoConstrains = new ColumnConstraints(200,200, Double.MAX_VALUE);
+        columnTwoConstrains.setHgrow(Priority.ALWAYS);
+        gp.getColumnConstraints().addAll(columnOneConstraints, columnTwoConstrains);
+        return gp;
     }
 
+    private void AddUI(GridPane gridPane){ //positioning is important when adding, treat like an array
+        // Add Header
+        Label headerLabel = new Label("Change Password");
+        headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        gridPane.add(headerLabel, 0,0,2,1);
+        GridPane.setHalignment(headerLabel, HPos.CENTER);
+        GridPane.setMargin(headerLabel, new Insets(20, 0,20,0));
 
-    public EditPassword() {
+        // Add Name Label
+        Label oldPassLabel = new Label("Enter Old Password: ");
+        gridPane.add(oldPassLabel, 0,1);
 
-    }
+        // Add Name Text Field
+        TextField oldPassField = new TextField();
+        oldPassField.setPrefHeight(40);
+        gridPane.add(oldPassField, 1,1);
 
-    @Override
-    public void actionPerformed(ActionEvent evt) {
-        char [] oldPassArr = enterOldPassword.getPassword();
-        char [] newPassArr = enterNewPassword.getPassword();
-        char [] reenteredNewPassArr = reenterNewPassword.getPassword();
-        String oldPassString = String.copyValueOf(oldPassArr);
-        String newPassString = String.copyValueOf(newPassArr);
-        String reenteredNewPassString = String.copyValueOf(reenteredNewPassArr);
-        //clear the text on gui
-        enterOldPassword.setText("");
-        enterNewPassword.setText("");
-        reenterNewPassword.setText("");
 
-        if(oldPassString.equals(newPassString)) {
-            successMessage.setText("New password must be different from old");
-        }
-        else if(!newPassString.equals(reenteredNewPassString)) {
-            successMessage.setText("Password is not the same as re-entered password");
-        }
-        else {
-            //take username and change password in db
-            successMessage.setText("Success!");
-        }
+        // Add Email Label
+        Label newPassLabel = new Label("Enter New Password: ");
+        gridPane.add(newPassLabel, 0, 2);
 
-        oldPassString = null;
-        newPassString = null;
-        reenteredNewPassString = null;
-    }
+        // Add Email Text Field
+        PasswordField newPassField = new PasswordField();
+        newPassField.setPrefHeight(40);
+        gridPane.add(newPassField, 1, 2);
 
-    public static void main(String[] args) {
-        EditPassword ep = new EditPassword();
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                ep.makeFrame();
+        // Add Password Label
+        Label newPass2Label = new Label("Re-enter New Password:");
+        gridPane.add(newPass2Label, 0, 3);
+
+        // Add Password Field
+        PasswordField newPass2Field = new PasswordField();
+        newPass2Field.setPrefHeight(40);
+        gridPane.add(newPass2Field, 1, 3);
+
+        // Add Submit Button
+        Button submitButton = new Button("Change Password");
+        submitButton.setPrefHeight(40);
+        submitButton.setDefaultButton(true);
+        submitButton.setPrefWidth(200);
+        gridPane.add(submitButton, 0, 4, 2, 1);
+        GridPane.setHalignment(submitButton, HPos.CENTER);
+
+        Text errorMessage = new Text("");
+        errorMessage.setFont(Font.font("Arial", FontWeight.BOLD, 16));
+        gridPane.add(errorMessage, 0 ,5, 2,1);
+        GridPane.setHalignment(errorMessage, HPos.CENTER);
+
+        submitButton.setOnAction(event -> {
+            //code for handling button clicks go here
+            //checkInput()
+            boolean validInput = checkInput(oldPassField, newPassField, newPass2Field);
+            if (!validInput) {
+                errorMessage.setFill(Color.RED);
+                errorMessage.setText("Please Provide Valid Input!");
             }
+            //changeInDatabase();
         });
-        return;
     }
 
-
+    public boolean checkInput(TextField oldPassField, PasswordField newPassField, PasswordField newPass2Field) {
+        String oldPass = oldPassField.getText();
+        oldPassField.setText("");
+        String newPass = newPassField.getText();
+        newPassField.setText("");
+        String newPass2 = newPass2Field.getText();
+        newPass2Field.setText("");
+        // check if old pass the same as account's old pass
+        if (!newPass.equals(newPass2)) {
+            return false;
+        }
+        return true;
+    }
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Create Account");
+        GridPane gp = createFormPane();
+        AddUI(gp);
+        Scene scene = new Scene(gp, 800,500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
