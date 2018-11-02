49a2a4538ae494ad75822f61a114d0b2037caf43
rishvanjay <rishvanjay@hotmail.com>
2018-11-01 20:33:00 -0400

    Stuff

--- WebCrawler/src/Core/TweetProcessingTest.java
+++ WebCrawler/src/Core/TweetProcessingTest.java
@@ -1,5 +1,56 @@
+package Core;
+
+import org.junit.Assert;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class TweetProcessingTest {
+
+    @org.junit.Test
+    public void mainProcess() throws IOException {
+        List<String> tweets = new ArrayList<>();
+        String name;
+        tweets.add("This is test 1");
+        tweets.add("This is test 2");
+        name = "test";
+        TweetProcessing tweetProcessing = new TweetProcessing();
+        int a = tweetProcessing.mainProcess(tweets, name);
+        Assert.assertNotEquals(null, a);
+    }
+
+    @org.junit.Test
+    public void processIBM() throws IOException {
+        List<String> tweets = new ArrayList<>();
+        String name;
+        tweets.add("This is test 1");
+        tweets.add("This is test 2");
+        name = "test";
+        TweetProcessing tweetProcessing = new TweetProcessing();
+        int a = tweetProcessing.processIBM(tweets);
+        Assert.assertNotEquals(null, a);
+    }
+
+    @org.junit.Test
+    public void processMS() throws IOException {
+        List<String> tweets = new ArrayList<>();
+        String name;
+        tweets.add("This is test 1");
+        tweets.add("This is test 2");
+        name = "test";
+
+        Documents documents = new Documents ();
+        int i = 1;
+        for (String s : tweets) {
+            documents.add("" + i++, "en", s);
+        }
+
+        TweetProcessing tweetProcessing = new TweetProcessing();
+        int a = tweetProcessing.processMS(documents);
+        Assert.assertNotEquals(null, a);
+    }
 
 }
