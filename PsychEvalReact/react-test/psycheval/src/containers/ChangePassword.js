803bfbcf068176c1948fed6a5a44b6ea2cce5cf8
Bryan Chiou <chioub@purdue.edu>
2018-12-06 01:16:05 -0500

    connected link w/ child and change password

--- PsychEvalReact/react-test/psycheval/src/containers/ChangePassword.js
+++ PsychEvalReact/react-test/psycheval/src/containers/ChangePassword.js
@@ -1,15 +1,114 @@
 import React, { Component } from "react";
 import { ButtonToolbar, Button } from "react-bootstrap";
+import firebase from "firebase";
 class ChangePassword extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPassword: "",
+      passwordOne: "",
+      passwordTwo: "",
+      error: ""
+    };
+  }
+
+  onSubmit = event => {
+    // eslint-disable-next-line
+    const { currentPassword, passwordOne, passwordTwo, error } = this.state;
+
+    const db = firebase.firestore();
+    db.settings({
+      timestampsInSnapshots: true
+    });
+    const email = window.localStorage.getItem("ParentEmail");
+    if (email === "") {
+      alert("please login again");
+    } else {
+      // TODO update database
+      var db1 = db.collection("Authentication");
+      db1.get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          if (
+            doc.data().email === email &&
+            doc.data().password === currentPassword
+          ) {
+            alert("password changed");
+            db1.doc(doc.id).set({
+              email: doc.data()["email"],
+              name: doc.data()["name"],
+              password: passwordOne,
+              type: doc.data()["type"]
+            });
+          }
+        });
+      });
+    }
+
+    this.setState({
+      currentPassword: "",
+      passwordOne: "",
+      passwordTwo: ""
+    });
+
+    event.preventDefault();
+  };
+
+  onChangePassword = event => {
+    // console.log(event.target.value);
+    this.setState({ currentPassword: event.target.value });
+  };
+
+  onChangePasswordOne = event => {
+    // console.log(event.target.value);
+    this.setState({ passwordOne: event.target.value });
+  };
+
+  onChangePasswordTwo = event => {
+    // console.log(event.target.value);
+    this.setState({ passwordTwo: event.target.value });
+  };
 
   goBack = async event => {
     this.props.history.push("/");
   };
   render() {
+    const { currentPassword, passwordOne, passwordTwo, error } = this.state;
+    const isInvalid =
+      currentPassword === "" ||
+      passwordOne === "" ||
+      passwordTwo === "" ||
+      passwordOne !== passwordTwo;
     return (
       <div>
-        Change Password
+        <form onSubmit={this.onSubmit}>
+          <input
+            name="currentPassword"
+            value={currentPassword}
+            onChange={this.onChangePassword}
+            type="password"
+            placeholder="Current Password"
+          />
+          <input
+            name="passwordOne"
+            value={passwordOne}
+            onChange={this.onChangePasswordOne}
+            type="password"
+            placeholder="New Password"
+          />
+          <input
+            name="passwordTwo"
+            value={passwordTwo}
+            onChange={this.onChangePasswordTwo}
+            type="password"
+            placeholder="Retype new Password"
+          />
+          <button disabled={isInvalid} type="submit">
+            Change Password
+          </button>
+
+          {error && <p>{error.message}</p>}
+        </form>
+
         <ButtonToolbar>
           <Button bsStyle="primary" bsSize="large" active onClick={this.goBack}>
             Back
--- PsychEvalReact/react-test/psycheval/src/containers/LinkWithChild.js
+++ PsychEvalReact/react-test/psycheval/src/containers/LinkWithChild.js
@@ -1,9 +1,125 @@
 import React, { Component } from "react";
+import { ButtonToolbar, Button } from "react-bootstrap";
+import firebase from "firebase";
 
 class LinkWithChild extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      ParentEmail: "",
+      CounselorEmail: "",
+      ChildName: "",
+      error: ""
+    };
+  }
+
+  onSubmit = event => {
+    const { ParentEmail, CounselorEmail, ChildName, error } = this.state;
+
+    const db = firebase.firestore();
+    db.settings({
+      timestampsInSnapshots: true
+    });
+    var CouncelorRef = db.collection("Counselor");
+    CouncelorRef.get().then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id, " => ", doc.data());
+        // console.log(CounselorEmail)
+        if (doc.data()["Email"] === CounselorEmail) {
+          console.log(
+            "Link with child True: ",
+            ChildName,
+            error,
+            ParentEmail,
+            CounselorEmail
+          );
+
+          var parents = doc.data().Parents;
+          var pcount = Object.keys(parents).length;
+          var newObj = Object.assign({}, parents);
+          var plz = [ParentEmail, false, ChildName, false];
+
+          var cRef = CouncelorRef.doc(doc.id);
+          newObj[pcount] = plz;
+          cRef.update({
+            Parents: firebase.firestore.FieldValue.delete()
+          });
+          cRef.update({
+            Parents: newObj
+          });
+        }
+      });
+    });
+
+    this.setState({
+      ParentEmail: "",
+      CounselorEmail: "",
+      ChildName: ""
+    });
+    alert("Your counselor will approve your request");
+    event.preventDefault();
+  };
+  goBack = async event => {
+    this.props.history.push("/");
+  };
+
+  onChangeParent = event => {
+    // console.log(event.target.value);
+    this.setState({ ParentEmail: event.target.value });
+  };
+
+  onChangeCounselor = event => {
+    // console.log(event.target.value);
+    this.setState({ CounselorEmail: event.target.value });
+  };
+
+  onChangeChild = event => {
+    // console.log(event.target.value);
+    this.setState({ ChildName: event.target.value });
+  };
+
   render() {
-    return <div>Link with child</div>;
+    const { ParentEmail, CounselorEmail, ChildName, error } = this.state;
+
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input
+            name="ParentEmail"
+            value={ParentEmail}
+            onChange={this.onChangeParent}
+            type="text"
+            placeholder="Parent's Email Address"
+          />
+          <br />
+          <input
+            name="CounselorEmail"
+            value={CounselorEmail}
+            onChange={this.onChangeCounselor}
+            type="text"
+            placeholder="Counselor's Email Address"
+          />
+          <br />
+          <input
+            name="ChildName"
+            value={ChildName}
+            onChange={this.onChangeChild}
+            type="text"
+            placeholder="Child's Name"
+          />
+          <br />
+          <button type="submit">Submit for Approval</button>
+
+          {error && <p>{error.message}</p>}
+        </form>
+        <ButtonToolbar>
+          <Button bsStyle="primary" bsSize="large" active onClick={this.goBack}>
+            Back
+          </Button>
+        </ButtonToolbar>
+      </div>
+    );
   }
 }
 
