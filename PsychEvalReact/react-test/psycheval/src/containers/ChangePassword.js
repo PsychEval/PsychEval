818da5b6211ac15c106e6da499f8927812ed3620
Bryan Chiou <chioub@purdue.edu>
2018-12-05 22:50:19 -0500

    created back button to go back to home page

--- PsychEvalReact/react-test/psycheval/src/containers/ChangePassword.js
+++ PsychEvalReact/react-test/psycheval/src/containers/ChangePassword.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
-
+import { ButtonToolbar, Button } from "react-bootstrap";
 class ChangePassword extends Component {
   state = {};
+
+  goBack = async event => {
+    this.props.history.push("/");
+  };
   render() {
-    return <div>Change Password</div>;
+    return (
+      <div>
+        Change Password
+        <ButtonToolbar>
+          <Button bsStyle="primary" bsSize="large" active onClick={this.goBack}>
+            Back
+          </Button>
+        </ButtonToolbar>
+      </div>
+    );
   }
 }
 
